85e5556dc36a82c7bed279596fd1367e95638cc3
Michaeldremy <michaeldremy@gmail.com>
2020-07-15 11:37:33 -0700

    Updates

---
+++ precache-manifest.e09901eeaa0cf77794493b855adc3721.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "088f29c985ba3a2670bae7e193063f47",
+    "url": "/test-deploy-project/index.html"
+  },
+  {
+    "revision": "26821177784ccf26e86b",
+    "url": "/test-deploy-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50c0dfb1d234dca10ea2",
+    "url": "/test-deploy-project/static/js/2.dc5cf456.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy-project/static/js/2.dc5cf456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26821177784ccf26e86b",
+    "url": "/test-deploy-project/static/js/main.5f7d7991.chunk.js"
+  },
+  {
+    "revision": "176d77308b3495f13002",
+    "url": "/test-deploy-project/static/js/runtime-main.b739d1d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
